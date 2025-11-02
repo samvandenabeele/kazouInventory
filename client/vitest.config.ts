ec164758cf5134b9c8e7675a8b2f184c4b918db8
Sam Vandenabeele <sam.vandenabeele.09@gmail.com>
2025-11-02 19:34:46 +0100

    feat: Add setupTests configuration for Vitest and mock window.matchMedia

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -7,8 +7,14 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    css: true,
     coverage: {
+      provider: 'v8',
       reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/setupTests.ts',
+      ],
     },
   },
 });
